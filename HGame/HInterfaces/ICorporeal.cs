d9f0ad7fbb20980953d090ba8084e27f0d53f5d4
harrebarre <maxilos84@gmail.com>
2018-11-02 16:21:45 +0100

    Implement basic Corporeal and Spatial interfaces.

---
+++ HGame/HInterfaces/ICorporeal.cs
@@ -0,0 +1,9 @@
+﻿using HGame.HShapes;
+
+namespace HGame.HInterfaces
+{
+    internal interface ICorporeal : ISpatial
+    {
+        Shape Shape { get; set; }
+    }
+}
---
+++ HGame/HInterfaces/ISpatial.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace HGame.HInterfaces
+{
+    internal interface ISpatial
+    {
+        Vector2 Position { get; set; }
+    }
+}
